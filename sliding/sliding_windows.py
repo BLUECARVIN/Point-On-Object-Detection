c3e873bea677b2eff26b7a4e141077b9845f2a07
BLUECARVIN <bluecarvin@163.com>
2018-05-16 21:45:55 +0800

    Add files via upload

---
+++ sliding/sliding_windows.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+
+def resize(img, scalFactor):
+    return cv2.resize(img, (int(img.shape[1] * (1 / scalFactor)),
+                            int(img.shape[0] * (1 / scalFactor))),
+                      interpolation=cv2.INTER_AREA)
+
+def pyramid(image, scale=1.5, minSize=(200, 80)):
+    yield image
+
+    while True:
+        image = resize(image, scale)
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+    yield image
+
+def sliding_window(image, stepSize, windowSize):
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            yield (x, y, image[y: y + windowSize[1], x: x + windowSize[0]])
+
+def non_max_suppression_fast(boxes, overlapThresh):
+    if len(boxes) == 0:
+        return []
+
+    if boxes.dtype.kind == 'i':
+        boxes = boxes.astype("float")
+
+    pick = []
+
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+    scores = boxes[:, 4]
+
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(scores)[::-1]
+
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        overlap = (w * h) / area[idxs[:last]]
+
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapThresh)[0])))
+
+    return boxes[pick].astype("int")
